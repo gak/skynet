537cdf30749dea56e50d9beb0a35cd921826ff70
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-10-21 10:44:52 +1100

    more opcodes, less string

--- skynet.py
+++ skynet.py
@@ -10,7 +10,15 @@ module = ast.Module()
 module.body = []
 
 def random_op():
-    return ast.Add()
+    r = random.randint(0, 3)
+    if r == 0:
+        return ast.Add()
+    if r == 1:
+        return ast.Sub()
+    if r == 2:
+        return ast.Mult()
+    if r == 3:
+        return ast.Div()
 
 def random_var():
     return ast.Name(id='a')
@@ -35,20 +43,17 @@ def random_state():
         return ast.Assign(targets=[random_var_store()], value=random_expr())
 
 def random_expr():
-    r = random.randint(0, 3)
+    r = random.randint(0, 2)
     if r == 0:
-        print 'str'
-        return ast.Str(s=random.random())
-    if r == 1:
         print 'num'
         return ast.Num(n=random.randint(0, 10))
-    if r == 2:
+    if r == 1:
         print 'binop'
         return ast.BinOp(
             left=random_expr(),
             op=random_op(),
             right=random_expr())
-    if r == 3:
+    if r == 2:
         print 'var'
         return random_var_load()
 
@@ -59,7 +64,7 @@ ast.fix_missing_locations(module)
 
 print '-' * 80
 print 'code:'
-print '-' * 80
+print '-' * 80,
 from unparse import Unparser
 up = Unparser(module)
 
