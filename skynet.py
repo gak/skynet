0813d0b2d6bd67b37793aafabc23808ec58b94c5
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-10-21 11:19:31 +1100

    functions

--- skynet.py
+++ skynet.py
@@ -33,14 +33,33 @@ def random_var_load():
     v.ctx = ast.Load()
     return v
 
+def random_func_name():
+    return 'func%i' % random.randint(0, 3)
+
+def random_func_var():
+    n = random_func_name()
+    return ast.Name(id=n, ctx=ast.Load())
+
 def random_state():
-    r = random.randint(0, 1)
+    r = random.randint(0, 3)
     if r == 0:
         print 'print expr'
         return ast.Print(values=[random_expr()], nl=True, dest=None)
     if r == 1:
         print 'assign'
         return ast.Assign(targets=[random_var_store()], value=random_expr())
+    if r == 2:
+        print 'def'
+        body = []
+        add_random_code(body, 2)
+        return ast.FunctionDef(body=body, decorator_list=[],
+                name=random_func_name(),
+                args=ast.arguments(args=[], kwarg=None, defaults=[],
+                    vararg=None))
+    if r == 3:
+        print 'call'
+        c = ast.Call(func=random_func_var(), args=[], keywords=[], starargs=None, kwargs=None)
+        return ast.Expr(value=c)
 
 def random_expr():
     r = random.randint(0, 2)
@@ -57,8 +76,11 @@ def random_expr():
         print 'var'
         return random_var_load()
 
-for a in range(10):
-    module.body.append(random_state())
+def add_random_code(body, count=10):
+    for a in xrange(count):
+        body.append(random_state())
+
+add_random_code(module.body)
 
 ast.fix_missing_locations(module)
 
