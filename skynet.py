165ac9e125e6b053fc1aebef6459a0a1f479352c
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-10-21 12:11:51 +1100

    better output

--- skynet.py
+++ skynet.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python
+from __future__ import division
 
-import pprint
 import random
 import ast
 
 import codegen
 
-module = ast.Module()
-module.body = []
-
 class Skynet:
 
+    def __init__(self):
+        self.attempts = 0
+        self.success = 0
+
+    def generate(self):
+        module = ast.Module()
+        module.body = []
+        self.add_random_code(module.body)
+        ast.fix_missing_locations(module)
+
+        if 1:
+            print '-' * 80
+            print 'code:'
+            print '-' * 80,
+            from unparse import Unparser
+            up = Unparser(module)
+
+        if 1:
+            print
+            print '-' * 80
+            print 'output:'
+            print '-' * 80
+
+        self.attempts += 1
+        try:
+            exec(compile(module, '<string>', 'exec'))
+            self.success += 1
+        except Exception, e:
+            print e
+
+    def main(self):
+        self.generate()
+            
     def random_op(self):
         r = random.randint(0, 3)
         if r == 0:
@@ -45,59 +75,35 @@ class Skynet:
     def random_state(self):
         r = random.randint(0, 3)
         if r == 0:
-            print 'print expr'
             return ast.Print(values=[self.random_expr()], nl=True, dest=None)
         if r == 1:
-            print 'assign'
             return ast.Assign(targets=[self.random_var_store()], value=self.random_expr())
         if r == 2:
-            print 'def'
             body = []
-            self.add_random_code(body, 2)
+            self.add_random_code(body, 3)
             return ast.FunctionDef(body=body, decorator_list=[],
                     name=self.random_func_name(),
                     args=ast.arguments(args=[], kwarg=None, defaults=[],
                         vararg=None))
         if r == 3:
-            print 'call'
             c = ast.Call(func=self.random_func_var(), args=[], keywords=[], starargs=None, kwargs=None)
             return ast.Expr(value=c)
 
     def random_expr(self):
         r = random.randint(0, 2)
         if r == 0:
-            print 'num'
             return ast.Num(n=random.randint(0, 10))
         if r == 1:
-            print 'binop'
             return ast.BinOp(
                 left=self.random_expr(),
                 op=self.random_op(),
                 right=self.random_expr())
         if r == 2:
-            print 'var'
             return self.random_var_load()
 
     def add_random_code(self, body, count=10):
         for a in xrange(count):
             body.append(self.random_state())
-
-    def main(self):
-        self.add_random_code(module.body)
-
-        ast.fix_missing_locations(module)
-
-        print '-' * 80
-        print 'code:'
-        print '-' * 80,
-        from unparse import Unparser
-        up = Unparser(module)
-
-        print
-        print '-' * 80
-        print 'output:'
-        print '-' * 80
-        exec(compile(module, '<string>', 'exec'))
 
 Skynet().main()
 
